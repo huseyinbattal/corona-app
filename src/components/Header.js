c6c2e12933ad5bd56342275c6eb61d38515ec407
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-20 10:34:15 +0300

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Logo from './Logo'
 
 function Header() {
   return (
       <nav className='bg-gray-800 border-gray-200 px-2'>
           <div className='container flex flex-wrap justify-between items-center mx-auto'>
-          Header
+          <Logo/>
           </div>
       
       
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-function Logo() {
+function Logo({ logo, title }) {
   return (
-    <div>Logo</div>
-  )
+    <div className="flex items-center">
+      <img src={logo} alt={title} className="mr-3 h-6 rounded-xl" />
+      <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
+        {title}
+      </span>
+    </div>
+  );
 }
 
 Logo.propTypes = {
-    
-}
+  title: PropTypes.string,
+  logo: PropTypes.string,
+};
 
 Logo.defaultProps = {
-    
-}
+  title: "Corona App",
+  logo: "https://media.istockphoto.com/vectors/stop-virus-corona-virus-monster-vector-logo-character-design-corona-vector-id1251479382",
+};
 
-
-export default Logo
+export default Logo;
