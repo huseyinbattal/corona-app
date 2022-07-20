f8921548c5a273fd6d2983aa5b86d8110df6a22e
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-20 12:55:59 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,16 @@ function Header() {
   return (
       <nav className='bg-gray-800 border-gray-200 px-2'>
           <div className='container flex flex-wrap justify-between items-center mx-auto'>
-          <Logo/>
+              <Logo />
+              <div className="w-full">
+                  <ul className='flex flex-row  mt-4 font-medium space-x-8'>
+                      <li>
+                          <a className='block cursor-pointer border-0 py-2 pr-4 pl-3 text-gray-400 hover:text-blue-700'>
+                              Merhaba
+                          </a>
+                      </li>
+                  </ul>
+              </div>
           </div>
       
       
