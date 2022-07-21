9af3c773d1c3981867efb471dff14865f6b8081f
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-21 11:16:40 +0300

    Create CoronaForCountry.js

---
+++ src/components/CoronaForCountry.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function CoronaForCountry() {
+    const [data, setData] = useState();
+  return (
+      <div>
+          
+    </div>
+  )
+}
+
+export default CoronaForCountry
