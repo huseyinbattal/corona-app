c0d506c089674703805246d09ac30a55d33848c7
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-21 08:43:54 +0300

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+const countryImage="https://okanhastanesi.com.tr/upload/icerik/corona-virusu-nedir-nasil-bulasir_20200124152509.jpg"
+
 
 function Card() {
   return (
-      <div className='max-w-sm'>
-          
+    <div className="max-w-sm rounded overflow-hidden shadow-md cursor-pointer hover:shadow-lg hover:scale-105 transition-all">
+      <img className="w-full" src={countryImage} alt="country" />
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
