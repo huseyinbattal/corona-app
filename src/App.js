a7f7d9e8542448ca6a85d95f64e320ba9e93a8f2
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-21 16:41:36 +0300

    fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Card from "./components/Card"
+import CoronaForCountry from "./components/CoronaForCountry";
 
 function App() {
   const [key, setKey] = useState(1);
@@ -13,7 +14,7 @@ function App() {
       />
 
       <div className="md:container md:mx-auto p-8  page-content ">
-        {key === 1 && <Card/>}
+        {key === 1 && <CoronaForCountry/>}
       </div>
     </>
   );
--- src/components/CoronaForCountry.js
+++ src/components/CoronaForCountry.js
@@ -1,12 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { API_TOKEN, API_URL } from "../consts/index";
 
 function CoronaForCountry() {
-    const [data, setData] = useState();
-  return (
-      <div>
-          
-    </div>
-  )
+  const [data, setData] = useState([]);
+  const [queryText, setQueryText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    const response = await axios.get(
+      `${API_URL}countriesData?country=${queryText}`,
+      {
+        headers: {
+              authorization: API_TOKEN,
+            
+        },
+      }
+    );
+      
+      setData(response.data.result)
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [queryText]);
+
+    return (
+        <>
+            <div className="mb-4">
+                <input value={queryText}/>
+            </div>
+        </>
+    )
+
 }
 
-export default CoronaForCountry
+export default CoronaForCountry;
