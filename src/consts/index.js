fd509b422698394057fd62fecfc054b99c942886
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-25 09:47:00 +0300

    Update index.js

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,2 +1,2 @@
 export const API_URL = "https://api.collectapi.com/corona/";
-export const API_TOKEN="apikey 38m3MhZtEwrrd4IbrW8pNZ:7d0GyxcLpH24A5WgBO8WCn"
+export const API_TOKEN="apikey 4x5Y1EbILDs96m79dj3i0T:7D9ziudsDDUMmPNZdPRBPS"
